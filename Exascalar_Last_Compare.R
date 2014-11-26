3d249abddea753295826c89bba46b3964fa8d5c2
Winston Saunders <winstonasaunders@gmail.com>
2014-11-26 11:36:44 -0800

    Updated file names

--- Exascalar_Last_Compare.R
+++ Exascalar_Last_Compare.R
@@ -1,7 +1,8 @@
-# Exascalar Data Trend Plot One
-
 ## This program imports cleaned data from the Green500 and Top500 lists
-## It plots one set of data and the trend of the top and median exascalar overlayed
+
+## there are several subtleties of the implenetation here which need to be paid attention to when adapting it.
+##each "500" list is called out explicitly
+##in a couple of places the number of lists (currently 10) is assumed.
 
 ## GET THE CLEANED DATA
 
@@ -35,67 +36,78 @@ Nov10 <- read.csv(paste0(results, "/Nov10.csv"), header=TRUE)
 Jun10 <- read.csv(paste0(results, "/Jun10.csv"), header=TRUE)
 Nov09 <- read.csv(paste0(results, "/Nov09.csv"), header=TRUE)
 Jun09 <- read.csv(paste0(results, "/Jun09.csv"), header=TRUE)
+#Nov08 <- read.csv(paste0(results, "/green500_top_200811.csv"), header=TRUE)
+#Jun08 <- read.csv(paste0(results, "/green500_top_200806.csv"), header=TRUE)
 
 print("data read")
 
 ##PLOT MODE, MEDIAN AND TOP EXASCALAR TREND
 
 ##Compute Mode Trend
+ModeEx <- rbind(Jun09[250,], Nov09[250,], Jun10[250,], Nov10[250,], Jun11[250,], Nov11[250,], Jun12[250,], Nov12[250,], Jun13[250,], Nov13[250,], Jun14[250,])
 
-TopEx <- rbind(Jun09[1,c(1:10)], Nov09[1,c(1:10)], Jun10[1,c(1:10)], Nov10[1,c(1:10)], Jun11[1,c(1:10)], Nov11[1,c(1:10)], Jun12[1,c(1:10)], Nov12[1,c(1:10)], 
-               Jun13[1,c(1:10)], Nov13[1,c(1:10)], Jun14[1,c(1:10)], Nov14[1,c(1:10)])
+TopEx <- rbind(Jun09[1,], Nov09[1,], Jun10[1,], Nov10[1,], Jun11[1,], Nov11[1,], Jun12[1,], Nov12[1,], Jun13[1,], Nov13[1,], Jun14[1,])
 
-
-mean_eff <- function(list){mean(list$rmax)/mean(list$power)}
-
-MeanEx <- matrix(c(mean_eff(Jun09), mean_eff(Nov09),
-                          mean_eff(Jun10), mean_eff(Nov10),
-                          mean_eff(Jun11), mean_eff(Nov11),
-                          mean_eff(Jun12), mean_eff(Nov12),
-                          mean_eff(Jun13), mean_eff(Nov13),
-                          mean_eff(Jun14), mean_eff(Nov14),
-                          mean(Jun09$rmax), mean(Nov09$rmax),
-                          mean(Jun10$rmax), mean(Nov10$rmax),
-                          mean(Jun11$rmax), mean(Nov11$rmax),
-                          mean(Jun12$rmax), mean(Nov12$rmax),
-                          mean(Jun13$rmax), mean(Nov13$rmax), 
-                          mean(Jun14$rmax), mean(Nov14$rmax)),
+MeanEx <- matrix(c(mean(Jun09$mflopswatt), mean(Nov09$mflopswatt),
+                   mean(Jun10$mflopswatt), mean(Nov10$mflopswatt),
+                   mean(Jun11$mflopswatt), mean(Nov11$mflopswatt),
+                   mean(Jun12$mflopswatt), mean(Nov12$mflopswatt),
+                   mean(Jun13$mflopswatt), mean(Nov13$mflopswatt),
+                   mean(Jun14$mflopswatt), mean(Nov14$mflopswatt),
+                   mean(Jun09$rmax), mean(Nov09$rmax),
+                   mean(Jun10$rmax), mean(Nov10$rmax),
+                   mean(Jun11$rmax), mean(Nov11$rmax),
+                   mean(Jun12$rmax), mean(Nov12$rmax),
+                   mean(Jun13$rmax), mean(Nov13$rmax), 
+                   mean(Jun14$rmax), mean(Nov14$rmax)),
                  ncol=2, nrow = 12)
 MeanEx <- as.data.frame(MeanEx)
 names(MeanEx) <- c("mflopswatt", "rmax")
 
-print("meanEx")
 
 ##
-median_eff <- function(list){median(list$rmax)/median(list$power)}
-
-MedianEx <- matrix(c(median_eff(Jun09), median_eff(Nov09),
-                       median_eff(Jun10), median_eff(Nov10),
-                       median_eff(Jun11), median_eff(Nov11),
-                       median_eff(Jun12), median_eff(Nov12),
-                       median_eff(Jun13), median_eff(Nov13),
-                       median_eff(Jun14), median_eff(Nov14),
-                       median(Jun09$rmax), median(Nov09$rmax),
-                       median(Jun10$rmax), median(Nov10$rmax),
-                       median(Jun11$rmax), median(Nov11$rmax),
-                       median(Jun12$rmax), median(Nov12$rmax),
-                       median(Jun13$rmax), median(Nov13$rmax), 
-                       median(Jun14$rmax), median(Nov14$rmax)),
+MedianEx <- matrix(c(median(Jun09$mflopswatt), median(Nov09$mflopswatt),
+                     median(Jun10$mflopswatt), median(Nov10$mflopswatt),
+                     median(Jun11$mflopswatt), median(Nov11$mflopswatt),
+                     median(Jun12$mflopswatt), median(Nov12$mflopswatt),
+                     median(Jun13$mflopswatt), median(Nov13$mflopswatt),
+                     median(Jun14$mflopswatt), median(Nov14$mflopswatt),
+                     median(Jun09$rmax), median(Nov09$rmax),
+                     median(Jun10$rmax), median(Nov10$rmax),
+                     median(Jun11$rmax), median(Nov11$rmax),
+                     median(Jun12$rmax), median(Nov12$rmax),
+                     median(Jun13$rmax), median(Nov13$rmax), 
+                     median(Jun14$rmax), median(Nov14$rmax)),
                    ncol=2, nrow = 12)
 MedianEx <- as.data.frame(MedianEx)
 names(MedianEx) <- c("mflopswatt", "rmax")
-
-print("medianEx")
 
 ##PlotMean over Exascalar Data
 ## EXASCALAR PLOT OVERLAYING TWO LISTS
 
 ## plots "reference" list first, then "list of current interest" is overlayed
-png(filename= "Exascalar_Trend.png", height=500, width=400)
+
+png(filename= "Exascalar_Last_Compare.png", height=500, width=400)
+
+plot(Jun14$mflopswatt ,
+     Jun14$rmax*10^3, 
+     log="xy", 
+     asp = 4/3.2, 
+     xlab = "Efficiency (mflops/watt)",
+     ylab = "Performance (mflops)", 
+     main = "Exascalar Evolution", 
+     col = "blue",
+     cex=0.9,
+     pch=1, 
+     xlim=c(10,100000), 
+     ylim=c(5*10^7,2*10^12))
+
+par(new=TRUE)
+
 plot(Nov14$mflopswatt ,
      Nov14$rmax*10^3, 
      log="xy", 
-     asp = 4/3.2, 
+     asp = TRUE, 
      xlab = "",
      ylab = "", 
      main = "", 
@@ -103,65 +115,25 @@ plot(Nov14$mflopswatt ,
      bg = "steelblue2",
      pch=21, 
      xlim=c(10,100000), 
-     ylim=c(1*10^7,2*10^12))
-par(new=TRUE)
-##This plots the "Median" trend line of efficiency and performance
-matplot(MedianEx$mflopswatt,
-     MedianEx$rmax*10^3,
-     type="l",
-     lty=1,
-     lwd=6,log="xy", 
-     asp = 4/3.2, 
-     xlab = "Efficiency (mflops/watt)",
-     ylab = "Performance (mflops)", 
-     main = "Exascalar", 
-     pch=19, 
-     cex = 0.6,
-     col = "darkred",
-     xlim=c(10,100000), 
-     ylim=c(1*10^7,2*10^12)
-     )
-par(new=TRUE)
-##plot the trend line of the "Top" exascalar system
-matplot(TopEx$mflopswatt,
-     TopEx$rmax*10^3, 
-     type = "l",
-     lty=1,
-     lwd=6,
-     log="xy", 
-     asp = 4/3.2, 
-     xlab = "",
-     ylab = "", 
-     main = "", 
-     pch=19, 
-     cex = 0.6,
-     col = "dark green",
-     xlim=c(10,100000), 
-     ylim=c(1*10^7,2*10^12))
+     ylim=c(5*10^7,2*10^12))
 
-##label Plot Lines
-
-text(TopEx$mflopswatt[10],
-     TopEx$rmax[10]*10^3, "Top", cex=.8, col="dark green", pos=3)
-
-text(MedianEx$mflopswatt[2],
-     MedianEx$rmax[2]*10^3, "Median", cex=.8, col="dark red", pos=4)
-
-
-text(1.0E4,
-     1.1E7, "November 2014 ", cex=.8, col="black", pos=3)
-text(1.0E4,
-     0.7E7, "Green500 and Top500 Lists", cex=.8, col="black", pos=3)
 
 ##add text to plots  (Some are commented out to clean up appearance, but left in for possible later convenience)
 
-##add text to plots  (Some are commented out to clean up appearance, but left in for possible later convenience)
+legend(4E1,1E11, legend = c("Nov 14","Jun 14"), pch=c(21,1), col = c("red", "blue"), 
+       pt.bg=c("steelblue", NULL), cex=0.7)
+
+text(1.0E4,
+     5.E7, "Nov 2014", cex=.8, col="black", pos=3)
+text(0.8E4,
+     3.3E7, "data from Green500 and Top500 Lists", cex=.6, col="black", pos=3)
+
 
 #text(0.35e+05, 2e+12, expression(epsilon == 0), cex=.7, srt=-45)
-text(0.35e+04, 1e+12, expression(epsilon == -1), cex=.9, srt=-45)
-text(1.2e+02, 1e+12, expression(epsilon == -2), cex=.9, srt=-45)
-text(0.2e+02, .25e+12, expression(epsilon == -3), cex=.9, srt=-45)
-text(0.15e+02, 1.3e+10, expression(epsilon == -4), cex=.9, srt=-45)
+text(0.35e+04, 1e+12, expression(epsilon == -1), cex=.7, srt=-45)
+text(1.2e+02, 1e+12, expression(epsilon == -2), cex=.7, srt=-45)
+text(0.2e+02, .25e+12, expression(epsilon == -3), cex=.7, srt=-45)
+text(0.15e+02, 1.3e+10, expression(epsilon == -4), cex=.7, srt=-45)
 
 text(0.15e+05, .5e+12, "20 MWatt", cex=.7, srt=45)
 text(0.15e+05, .5e+10, "0.2 MWatt", cex=.7, srt=45)
@@ -201,4 +173,3 @@ isoexaline <- function(exascalar, efficiencyrangelow=0.5*20, efficiencyrangehigh
 for (i in 0:7) {lines(isoexaline(-i)[,1], isoexaline(-i)[,2], lwd=.5, lty=2)}
 
 dev.off()
-
